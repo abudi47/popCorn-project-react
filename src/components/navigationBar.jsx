8fe8d2a7800076987dfe76b32c64ba83837a4269
Abudi <abduselamt47@gmail.com>
2025-02-27 18:41:35 +0300

    updated

--- src/components/navigationBar.jsx
+++ src/components/navigationBar.jsx
@@ -1,26 +1,55 @@
 import React, { useState } from 'react'
 
 export const NavigationBar = ({tempMovieData,tempWatchedData }) => {
-  const [movies, setMovies] = useState(tempMovieData);
   
-  const [query , setQuery] = useState("")
   return (
     <nav className="nav-bar">
-        <div className="logo">
+        <Logo />
+        
+        <Search />
+
+        <NumResult tempMovieData={tempMovieData}/>
+        
+       
+      </nav>
+    
+  )
+}
+
+
+function Logo () {
+  return (
+    <div className="logo">
           <span role="img">🍿</span>
           <h1>usePopcorn</h1>
         </div>
-        <input
+
+  )
+
+
+}
+
+function NumResult ({tempMovieData}) {
+  const [movies, setMovies] = useState(tempMovieData);
+
+
+  return (
+    <p className="num-results">
+          Found <strong>{movies.length}</strong> results
+        </p>
+  )
+}
+function Search () {
+  const [query , setQuery] = useState("")
+
+  return (
+    <input
           className="search"
           type="text"
           placeholder="Search movies..."
           value={query}
           onChange={(e) => setQuery(e.target.value)}
         />
-        <p className="num-results">
-          Found <strong>{movies.length}</strong> results
-        </p>
-      </nav>
-    
+
   )
-}
+} 
