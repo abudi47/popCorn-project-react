912f57ac7701485ae8966d7a1e8684c5834dcb8a
Abudi <abduselamt47@gmail.com>
2025-03-01 11:00:41 +0300

    updated star reating

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 // import './index.css'
 // import App from './App.jsx'
 
-import StarRating from './components/StarRating'
-createRoot(document.getElementById('root')).render(
+import StarRating from "./components/StarRating";
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <StarRating maxRating={5}/>
-    <StarRating maxRating={10}/>
-  </StrictMode>,
-)
+    <StarRating
+      maxRating={5}
+      message={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+      defaultRating={3}
+    />
+    <StarRating maxRating={10} color="red" size="40" />
+  </StrictMode>
+);
